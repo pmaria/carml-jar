ee613bb4e51817d6195bef9706f99665ca3e2309
Pano Maria <pano.maria@gmail.com>
2020-10-08 17:19:14 +0200

    update to latest carml version and rework cli

---
+++ src/main/java/com/skemu/rdf/carml/CarmlJarException.java
@@ -0,0 +1,13 @@
+package com.skemu.rdf.carml;
+
+public class CarmlJarException extends RuntimeException {
+
+  public CarmlJarException(String message) {
+    super(message);
+  }
+
+  public CarmlJarException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
